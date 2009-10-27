7a66e15a5b7578aa1a0c9d28b9bc09532986f76d
Jean-Philippe Bougie <jp.bougie@gmail.com>
2009-10-27 16:03:56 -0400

    use the body id to modularize, and view of the iterations on a project

--- lib/routes/project.rb
+++ lib/routes/project.rb
@@ -29,6 +29,34 @@ module Sinatra
       iteration ||= alternative.iteration
       project_url(project || alternative.iteration.project, alternative.id, (!iteration.current? || permalink) ? iteration : nil)
     end
+    
+    def distance_of_time(from, to=nil)
+      to ||= Time.now
+      delta = to.to_i - from.to_i
+      
+      case delta
+        when 0..60    then "less than a minute"
+        when 60..119  then "a minute"
+        when 61..3599 then "#{delta / 1.minute} minutes"
+        else case (delta / 1.hour)
+          when 1 then "an hour"
+          when 2..23 then "#{delta / 1.hour} hours"
+          else case (delta / 1.day)
+            when 1 then "a day"
+            when 2..7 then "#{delta / 1.day} days"
+            else case (delta / 7.days)
+              when 1 then "a week"
+              when 2..4 then "#{delta / 7.days} weeks"
+              else case (delta / 1.month)
+                when 1 then "a month"
+                else "#{delta / 1.month} months"
+              end
+            end
+          end
+        end
+      end
+    end
+    
   end
   
   module Project
@@ -114,6 +142,15 @@ module Sinatra
         
         redirect project_url(@project)
       end
+      
+      app.get "/:user/:project/iterations" do
+        login_required
+        @project = get_project_or_404(params[:user], params[:project])
+
+        halt(403) unless authorized_for_project? current_user, @project
+        
+        haml :iterations
+      end
 
       # /jpbougie/project-birch/
       # /jpbougie/project-birch/a0234a0
@@ -137,7 +174,7 @@ module Sinatra
 
           not_found("Iteration could not be found") if @iteration.nil?
 
-          haml :project
+          haml :project, :locals => {:bodyid => "project-section"}
         end
       end
       
